ce712877fde33c216fdb97bbd82271644fe1fc37
Rafael Cavicchio <rafael.cavicchio@gmail.com>
2025-09-15 22:10:59 -0300

    new push

---
+++ capitulo_05/estatistica.py
@@ -0,0 +1,15 @@
+#Estatística abrange conceitos matemáticos e técnicas para compreendermos os dados
+from collections import Counter
+import matplotlib.pyplot as plt
+
+num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+friends_counts = Counter(num_friends)
+xs = range(101) #o maior valor é 100
+ys = [friends_counts[x] for x in xs] #a altura indica o numero de amigos
+plt.bar(xs, ys)
+plt.axis([0, 101, 0, 25]) #limites dos eixos x e y
+plt.title("Histograma de número de amigos")
+plt.xlabel("# de amigos")
+plt.ylabel("# de pessoas") 
+plt.show()
