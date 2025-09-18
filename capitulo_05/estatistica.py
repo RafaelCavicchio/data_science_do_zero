ceeb407121f2cb098c784f1354f0f258a4cc5c66
Rafael Cavicchio <rafael.cavicchio@icl-group.com>
2025-09-18 17:18:03 -0300

    a

--- capitulo_05/estatistica.py
+++ capitulo_05/estatistica.py
@@ -2,6 +2,10 @@
 from collections import Counter
 import matplotlib.pyplot as plt
 from typing import List
+import sys
+import os
+sys.path.append(os.getcwd())
+from capitulo_04.algebra_linear import sum_of_squares
 
 num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
@@ -31,7 +35,8 @@ def grafico_para_apresentacao(num_friends):
 
 #region MÉDIA
 def mean(xs: List[float]) -> float:
-    '''Retorna a média aritmética dos valores em xs.
+    '''Média \n
+    Retorna a média aritmética dos valores em xs.
     é influênciada pelo valor dos outliers(valores atípicos) e se o valor do índice 1 for aumentado, a média também aumenta.
     '''
     return sum(xs) / len(xs)
@@ -41,7 +46,7 @@ def mean(xs: List[float]) -> float:
 
 #mediana com elementos ímpares
 def _median_odd(xs: List[float]) -> float:
-    '''Retorna a mediana dos valores em xs quando xs tem um número ímpar(ODD) de elementos.
+    '''MEDIANA Retorna a mediana dos valores em xs quando xs tem um número ímpar(ODD) de elementos.
     é o valor do índice do meio quando os dados estão ordenados, não é influenciada por outliers, se valor de alguma posição mudar,
      o índice do meio continua sendo o mesmo.
     '''
@@ -59,7 +64,13 @@ def _median_even(xs: List[float]) -> float:
 
 #mediana geral
 def median(v: List[float]) -> float:
-    '''Retorna a mediana dos valores em v, seja v par ou ímpar.'''
+    '''MEDIANA -> Retorna a mediana dos valores em v, seja v par ou ímpar.
+    verifica se o número de elementos do array é par ou impar e chama a função correta para calcular a mediana.
+    Args:
+        v (List[float]): Array contendo os dados
+    Returns:
+        float: Valor do índice que se encontra no meio do array (50% do array)
+    '''
     return _median_even(v) if len(v) % 2 == 0 else _median_odd(v)
 #assert median([1, 10, 2, 9, 5]) == 5
 #assert median([1, 9, 2, 10]) == (2+9) / 2
@@ -70,10 +81,15 @@ def median(v: List[float]) -> float:
 '''quantis são valores que dividem um conjunto de dados ordenados em partes iguais.'''
 
 def quantile(xs: List[float], p: float) -> float:
-    '''Retorna o p-ésimo quantil dos valores em xs.
-    separa uma determinada porcentagem dos dados ordenados.
-    Por exemplo, o 0,5-ésimo quantil é a mediana(50%).
-    '''
+    '''QUANTIL -> separa uma determinada porcentagem dos dados ordenados.
+    Por exemplo, o 0.5-ésimo quantil é a mediana(50%).
+
+    Args:
+        xs (List[float]): Array contendo os dados.
+        p (float): porcentagem dos dados que queremos, ex: 0.25
+    Returns:
+        float: Valor que representa o p-ésimo quantil de xs.    
+    '''    
     p_index = int(p * len(xs)) #o índice do p-ésimo quantil
     return sorted(xs)[p_index]
 #assert quantile(num_friends, 0.10) == 1
@@ -86,7 +102,8 @@ def quantile(xs: List[float], p: float) -> float:
 '''moda é o valor que aparece com mais frequência em um conjunto de dados.'''
 
 def mode(x: List[float]) -> List[float]:
-    '''Retorna uma lista pois pode haver mais de uma moda.'''
+    '''Moda \n
+    Retorna uma lista pois pode haver mais de uma moda.'''
     counts = Counter(x) #um dicionário de valor -> contagem
     max_count = max(counts.values()) #a maior contagem
     return [x_i for x_i, count in counts.items() if count == max_count] #todos os valores com a maior contagem
@@ -100,8 +117,50 @@ def mode(x: List[float]) -> List[float]:
 
 #region AMPLITUDE
 def data_range(xs: List[float]) -> float:
-    '''Retorna a diferença entre o maior e o menor valor em xs.'''
+    '''AMPLITUDE
+    Args:
+        xs (List[float]): Array contendo os dados.
+    Returns:
+        float: Retorna a diferença entre o maior e o menor valor em xs
+    '''
     return max(xs) - min(xs)
+#endregion
+
+#region DESVIO
+def de_mean(xs: List[float]) -> List[float]:
+    '''DESVIO -> calculamos a média do array e subtraímos os valores do array com a média, assim temos o desvio.
+    Args:
+        xs (List[Float]): Array com os dados.
+    Returns:
+        List[Float]: Nova array contendo o desvio
+    '''
+    x_bar = mean(xs)
+    return [x - x_bar for x in xs]
+#endregion
+
+#region VARIÂNCIA
+def variance(xs: List[float]) -> float:
+    '''VARIÂNCIA -> é a média dos quadrados dos desvios. \n
+    A variância mede quanto os dados se espalham em relação à média \n
+    Formula:
+        VAR(X) = (1/(n-1)) * Σ(xi - x̄)²
+    xi: valores da amostra
+    x̄: média da amostra
+    n: número de elementos na amostra
+    1/(n-1): graus de liberdade, usado para corrigir o viés na estimativa da variância populacional a partir de uma amostra.
+    ²: calcumamos o quadrado dos desvios para eliminar dados negativos e dar mais peso aos desvios maiores.
+        
+    Args:
+        xs (List[float]): Array contendo os dados.
+    Returns:
+        float: Retorna a variância dos valores do array.
+    '''
+    assert len(xs) >=2, "A variancia requer ao menos 2 elementos no array"
+
+    n = len(xs)
+    deviations = de_mean(xs)
+    return sum_of_squares(deviations) / (n - 1) #dividimos por n-1 e não n para corrigir o viés na estimativa da variância populacional
+assert 81.54 < variance(num_friends) < 81.55
 #endregion
 
 #endregion
